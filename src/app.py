79e9d8c754a75d4288c98012bb3202ab8275f110
EgosPWD <egossolus@gmail.com>
2025-08-12 23:22:29 -0400

    change and save system prompt

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from models.gemini import  function_model as gemini_model
 from models.open_router import function_model as qwen_model
+from utils.system_promt import load_system_prompt, save_system_prompt
 import argparse
 
 load_dotenv(dotenv_path=".env")
@@ -11,21 +12,29 @@ parser = argparse.ArgumentParser("Zia")
 parser.add_argument("-q", "--qwen", help="Use a Qwen", action="store_true")
 parser.add_argument("-g", "--gemini", help="Use a Gemini", action="store_true")
 parser.add_argument("model", type=str, help="model", nargs="?")
+parser.add_argument("--set-prompt", type=str, help="change and save the system prompt")
 
 args = parser.parse_args()
 
+if args.set_prompt:
+    save_system_prompt(args.set_prompt)
+    system_prompt = args.set_prompt
+else:
+    system_prompt = load_system_prompt()
+
+
 if args.qwen:
     if args.model:
-        qwen_model(qwen_model==args.model, system_prompt=os.getenv("SYSTEM_PROMPT"))
+        qwen_model(qwen_model==args.model, system_prompt=system_prompt)
     else:
-        qwen_model(system_prompt=os.getenv("SYSTEM_PROMPT"))
+        qwen_model(system_prompt=system_prompt)
 elif args.gemini:
     if args.model:
-        gemini_model(google_model=args.model, system_prompt=os.getenv("SYSTEM_PROMPT"))
+        gemini_model(google_model=args.model, system_prompt=system_prompt)
     else:
-        gemini_model(system_prompt=os.getenv("SYSTEM_PROMPT"))
+        gemini_model(system_prompt=system_prompt)
 else:
     if args.model:
-        gemini_model(google_model=args.model, system_prompt=os.getenv("SYSTEM_PROMPT"))
+        gemini_model(google_model=args.model, system_prompt=system_prompt)
     else:
-        gemini_model(system_prompt=os.getenv("SYSTEM_PROMPT"))
+        gemini_model(system_prompt=system_prompt)
--- src/utils/system_promt.py
+++ src/utils/system_promt.py
@@ -1,2 +1,10 @@
 import os
-PROMPT_FILE = "../system_prompt.txt"
+PROMPT_FILE = "src/config/system_prompt.txt"
+def load_system_prompt():
+    with open(PROMPT_FILE, "r", encoding="utf-8") as f:
+        return f.read().strip()
+def save_system_prompt(new_prompt):
+    with open(PROMPT_FILE,"w", encoding="utf-8") as f:
+        f.write(new_prompt.strip())
+
+
